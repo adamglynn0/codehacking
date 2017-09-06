9327b5835208ad120c74a86bfd7e4186fbbf65af
Adam Glynn <adamglynn@Adams-MacBook-Pro.local>
2017-09-06 15:16:49 +0100

    my first commit - admin views created

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adamglynn
+ * Date: 9/6/17
+ * Time: 3:06 PM
+ */
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adamglynn
+ * Date: 9/6/17
+ * Time: 3:12 PM
+ */
