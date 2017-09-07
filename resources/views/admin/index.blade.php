fab93354914a99c76cc622e2647fe6c2f8692a77
Adam Glynn <adamglynn@Adams-MacBook-Pro.local>
2017-09-07 13:45:41 +0100

    admin master page set up

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: adamglynn
- * Date: 9/6/17
- * Time: 3:06 PM
- */
+@extends('layouts.admin')
+
+
+
+
+@section('content')
+
+
+    <h1>Admin</h1>
+
+
+
+@stop
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,4 +1,7 @@
 <?php
+
+
+        echo "Hello";
 /**
  * Created by PhpStorm.
  * User: adamglynn
